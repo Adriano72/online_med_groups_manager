5b41e40efb0b029344700fd686f7988ec02915d3
Adriano Massi <adrianomassi@gmail.com>
2018-04-21 03:36:17 +0200

    Group list working

--- client/components/groups/groups_list.js
+++ client/components/groups/groups_list.js
@@ -3,8 +3,24 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Link, browserHistory } from 'react-router';
 import { Groups } from '../../../imports/collections/groups';
 
-
 class GroupsList extends Component {
+  renderRows() {
+      return this.props.groups.map(group => {
+        const { _id, group_language, group_leader, meet_time  } = group;
+        const leader = group_leader.first_name + " " + group_leader.last_name;
+        const meetingtime = meet_time.day_of_week + " at " + meet_time.meet_time;
+
+        return (
+          <tr key={_id}>
+            <td>{group_language}</td>
+            <td>{leader}</td>
+            <td>{meetingtime}</td>
+            <td>Detail</td>
+          </tr>
+        )
+      });
+  }
+
   render() {
     return (
       <div className="container-fluid top-buffer">
@@ -12,12 +28,15 @@ class GroupsList extends Component {
           <table className="table">
             <thead>
               <tr>
-                <th>Group Name</th>
+                <th>Language</th>
                 <th>Group Leader</th>
                 <th>Meeting Time</th>
                 <th>Detail</th>
               </tr>
             </thead>
+            <tbody>
+              {this.renderRows()}
+            </tbody>
           </table>
         </pre>
       </div>
@@ -27,6 +46,5 @@ class GroupsList extends Component {
 
 export default createContainer(() => {
   Meteor.subscribe('groups');
-
-  return { groups: Groups.find({}).fetch() };
+  return { groups: Groups.find({}, { sort: { group_language: -1 } }).fetch() };
 }, GroupsList);
