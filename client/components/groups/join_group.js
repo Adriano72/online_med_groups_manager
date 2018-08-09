a8bd35d35915e8c8b09732b01199870da72487ae
Adriano Massi <adrianomassi@gmail.com>
2018-08-09 02:40:54 +0200

    Pubblicazione su Galaxy, aggiunte privacy policy, cookies policy e cookie solution (Iubenda). E GDPR su join group

--- client/components/groups/join_group.js
+++ client/components/groups/join_group.js
@@ -25,8 +25,9 @@ class JoinGroup extends Component {
     e.preventDefault();
     let name = document.getElementById("full_name").value;
     let email = document.getElementById("email").value;
+    let gdpr = this.refs.gdpr_consent.checked
 
-    if(name == "" || email == "" || this.state.country == '') {
+    if(name == "" || email == "" || this.state.country == '' || !gdpr) {
       console.log("Name: ", name, " Email: ", email, " Country: ", this.state.country);
       Alert.error('Name, Email, Password and Country are mandatory fields!', {
         position: 'top-left',
@@ -38,20 +39,13 @@ class JoinGroup extends Component {
       return;
     }
 
-    var newMeditator = {
-     username: name,
-     email: email,
-     country: this.state.country
-    };
-
-    console.log("COUNTRY: ", newMeditator.country);
-
     var selectedGroup = new Group(this.props.groups);
 
     const meditators = {
       full_name: name,
       country: this.state.country,
-      email: email
+      email: email,
+      gdpr_ok: gdpr
     }
 
     selectedGroup.addMeditator(meditators);
@@ -84,35 +78,47 @@ class JoinGroup extends Component {
         <div className="panel panel-primary">
           <div className="panel-heading">Join online group</div>
           <div className="panel-body">
-          <div className="form-group">
-            <input type="text" id="full_name"
-                  className="form-control input-lg" placeholder="Full name"
-            />
-          </div>
-          <div className="form-group">
-            <CountryDropdown
-              value={this.state.country}
-              classes='countryDropDown'
-              defaultOptionLabel = 'select country'
-              onChange={(val) => this.updateCountry(val)}
-            />
-          </div>
-          <div className="form-group">
-            <input type="email" id="email"
-                  className="form-control input-lg" placeholder="email"
-            />
-          </div>
-          <button
-            className="btn btn-primary"
-
-            onClick={this.handleSubmit.bind(this)}
-            >
-            Join Group
-          </button>
-        <Alert stack={{limit: 3}} />
+            <div className="form-group">
+              <input type="text" id="full_name"
+                    className="form-control input-lg" placeholder="Full name"
+              />
+            </div>
+            <div className="form-group">
+              <CountryDropdown
+                value={this.state.country}
+                classes='countryDropDown'
+                defaultOptionLabel = 'select country'
+                onChange={(val) => this.updateCountry(val)}
+              />
+            </div>
+            <div className="form-group">
+              <input type="email" id="email"
+                    className="form-control input-lg" placeholder="email"
+              />
+            </div>
+          <Alert stack={{limit: 3}} />
+        </div>
+        </div>
+        <div className="panel panel-success">
+          <div className="panel-heading">Communication Permissions</div>
+            <div className="panel-body">
+                <div className="form-group">
+                  <div className="checkbox">
+                    <p>WCCM will use the information you provide on this form to be in touch with you and to provide updates and marketing. Please let us know that you would like to hear from us:</p>
+                    <br />
+                    <label>
+                      <input type="checkbox" ref="gdpr_consent" value="" />
+                      <b>I agree to be contacted by EMAIL</b>
+                    </label><br /><br />
+                    <p>You can change your mind at any time by clicking the unsubscribe link in the footer of any email you receive from us, or by contacting us at leonardo@wccm.org. We will treat your information with respect. For more information about our privacy practices please visit our website. By clicking below, you agree that we may process your information in accordance with these terms.</p>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <button className="btn btn-primary" onClick={this.handleSubmit.bind(this)}>
+          Join Group
+        </button>
       </div>
-      </div>
-    </div>
     );
   }
 }
